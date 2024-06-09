af7fd65b3b601aee4ebb5b14938ddab0885a7488
bryanDev <brayan7br7@gmail.com>
2024-06-09 16:12:37 -0500

    corrigiendo error de escritura

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import NavBar from "@/components/nav-menu/NavBar";
+import NavBar from "@/components/navMenu/NavBar";
 import "../styles/globals.css";
 
 import { cn } from "@/lib/utils";
@@ -22,13 +22,11 @@ export default function RootLayout({
     <html lang="es">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased bg-gray-50",
+          "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
       >
-        <div className="flex flex-col p-2">
         <NavBar />
-        </div>
         {children}
       </body>
     </html>

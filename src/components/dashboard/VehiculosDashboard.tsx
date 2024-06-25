69f02ae0c8f825e5126b61edad4ba3f84543eab7
bryanDev <brayan7br7@gmail.com>
2024-06-25 13:18:28 -0500

    corrgiendo boton de eliminar

--- src/components/dashboard/VehiculosDashboard.tsx
+++ src/components/dashboard/VehiculosDashboard.tsx
@@ -1,10 +1,13 @@
 "use client";
+
 import { IoAddCircleOutline } from "react-icons/io5";
 import { FaRegEdit } from "react-icons/fa";
 import Link from "next/link";
-import ButtonDelete from "../vehiculo/ButtonDelete";
 import { PiCurrencyDollarSimpleFill } from "react-icons/pi";
+import { MdDeleteOutline } from "react-icons/md";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
 import { fetchData } from "@/services/vehiculo";
 import { Vehicle } from "@/interfaces/Vehiculo";
 
@@ -25,6 +28,48 @@ export default function VehiculosDashboard() {
 
     fetchDataAsync();
   }, [refreshData]); // Añadir refreshData como dependencia
+
+  const handleDelete = async (idVehicle: string) => {
+    const result = await Swal.fire({
+      title: "¿Estas segura de eliminar este vehiculo?",
+      text: "Se borrará permanentemente",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, quiero eliminar",
+    });
+
+    if (result.isConfirmed) {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await axios.delete(
+          `http://localhost:8080/vehicle/delete/${idVehicle}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.status === 200) {
+          await Swal.fire({
+            title: "Eliminado!",
+            text: "Vehiculo eliminado correctamente",
+            icon: "success",
+          });
+          setRefreshData(prev => !prev); // Actualizar el estado para forzar el re-render
+        }
+      } catch (error) {
+        console.error("Error deleting vehicle:", error);
+        Swal.fire({
+          title: "Error!",
+          text: "No se pudo eliminar el vehiculo",
+          icon: "error",
+        });
+      }
+    }
+  };
 
   return (
     <>
@@ -79,7 +124,12 @@ export default function VehiculosDashboard() {
                     >
                       <FaRegEdit size={15} />
                     </Link>
-                    <ButtonDelete idVehicle={auto.id} />
+                    <button
+                      className="btn btn-error shadow-lg btn-sm mx-3 text-white"
+                      onClick={() => handleDelete(auto.id)}
+                    >
+                      <MdDeleteOutline size={15} />
+                    </button>
                   </td>
                 </tr>
               ))}
